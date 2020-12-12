54af05887432496adf57ad4105cd46b0bb301128
DevasyaAg8 <69330926+DevasyaAg8@users.noreply.github.com>
2020-12-12 16:59:38 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,20 @@
+var database,playerCount,gameState=0,form,player,game,allPlayers;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    game=new Game()
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    if (playerCount===4){
+        game.update(1);
+    }
+    if (gameState===1){
+        clear();
+        game.play();
+    }
+}
+
